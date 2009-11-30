285e772316b874dd4a745ecb6f0ad40f95385c44
Joshua Hull <joshbuddy@gmail.com>
2009-11-30 16:23:43 -0500

    better syntax

--- lib/meddler.rb
+++ lib/meddler.rb
@@ -5,9 +5,9 @@ require 'meddler/builder'
 
 class Meddler
 
-  def initialize(app, before_rule, after_rule, wrapped_app)
-    wrapped_app.run(PostInterceptor.new(app, after_rule))
-    @app = PreInterceptor.new(wrapped_app.to_app, app, before_rule)
+  def initialize(app, on_request, on_response, before, after, wrapped_app)
+    wrapped_app.run(PostInterceptor.new(app, on_response, after))
+    @app = PreInterceptor.new(wrapped_app.to_app, app, on_request, before)
   end
   
   def call(env)
@@ -19,33 +19,43 @@ class Meddler
   
 
   class PostInterceptor
-    def initialize(app, rule = nil)
+    attr_reader :app, :rules, :filters
+    def initialize(app, rules, filters)
       @app = app
-      @rule = rule
+      @rules = rules
+      @filters = filters
     end
     
     def call(env)
-      response = @app.call(env)
-      if @rule.nil? || @rule.call(Rack::Response.new(response))
-        response
+      raw_response = app.call(env)
+      response = Rack::Response.new(raw_response[2], raw_response[0], raw_response[1])
+      if rules.nil? || rules.all?{|r| r.call(response)}
+        filters && filters.each{|f| f.call(response)}
+        response.to_a
       else
-        throw :skipped_middleware, response
+        throw :skipped_middleware, response.to_a
       end
     end
 
   end
-  
+
   class PreInterceptor
-    attr_accessor :skip_app
-    
-    def initialize(app, skip_app, rule = nil)
+    attr_reader :app, :skip_app, :rules, :filters
+    def initialize(app, skip_app, rules, filters)
       @app = app
       @skip_app = skip_app
-      @rule = rule
+      @rules = rules
+      @filters = filters
     end
-    
+
     def call(env)
-      response = @rule.nil? || @rule.call(Rack::Request.new(env)) ? @app.call(env) : @skip_app.call(env)
+      request = Rack::Request.new(env)
+      if rules.nil? || rules.all?{|f| f.call(request)}
+        filters && filters.each{|f| f.call(request)}
+        app.call(env)
+      else
+        skip_app.call(env)
+      end
     end
   end
   
--- lib/meddler/builder.rb
+++ lib/meddler/builder.rb
@@ -3,10 +3,35 @@ class Meddler
 
     def initialize(app, rules = nil, &block)
       @app = app
-      @target = Rack::Builder.new(&block)
-      @meddler = Meddler.new(@app, rules && rules[:on_request], rules && rules[:on_response], @target)
+      @target = Rack::Builder.new{}
+      instance_eval(&block)
+      @meddler = Meddler.new(@app, @on_request, @on_response, @before, @after, @target)
     end
-
+    
+    def method_missing(method, *args, &block)
+      @target.send(method, *args, &block)
+    end
+    
+    def on_request(&block)
+      @on_request ||= []
+      @on_request << block
+    end
+      
+    def on_response(&block)
+      @on_response ||= []
+      @on_response << block
+    end
+      
+    def before(&block)
+      @before ||= []
+      @before << block
+    end
+    
+    def after(&block)
+      @after ||= []
+      @after << block
+    end
+    
     def call(env)
       @meddler.call(env)
     end
